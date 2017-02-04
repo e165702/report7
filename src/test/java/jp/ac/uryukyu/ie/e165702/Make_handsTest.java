acae9ce2bde70fe87ae93dc9950199377a8b8149
Tomohisa Oseko <e165702@ie.u-ryukyu.ac.jp>
2017-02-05 02:17:10 +0900

    一部改良

--- src/test/java/jp/ac/uryukyu/ie/e165702/Make_handsTest.java
+++ src/test/java/jp/ac/uryukyu/ie/e165702/Make_handsTest.java
@@ -13,23 +13,17 @@ public class Make_handsTest {
      */
     @Test
     public void make_hands_test() throws Exception {
-        int test_hands;
-        //int i3_d = 0;
-        int[] hands_list = new int[3];
-        boolean  not_duplication= true;
         Make_hands make_hands = new Make_hands();
-
         for (int i = 0; i <=2;) {
-            make_hands.hands1 = (int) (Math.random() * 3);
-            make_hands.hands_list[make_hands.i3_d] = make_hands.hands1;
+            make_hands.random_number = (int) (Math.random() * 3);
+            make_hands.hands_list[make_hands.count_number] = make_hands.random_number;
             //同じカードが出たかどうかを判定
-            if (make_hands.i3_d != 0) {
+            if (make_hands.count_number != 0) {
                 make_hands.Human_hands2();
             }
             if (make_hands.not_duplication==true) {
-                System.out.printf("引いたカードは%s\n",make_hands.hands1);
-                //System.out.printf("%s,%s,%s\n",make_hands.hands_list[0],make_hands.hands_list[1],make_hands.hands_list[2]);
-                make_hands.i3_d++;
+                System.out.printf("引いたカードは%s\n",make_hands.random_number);
+                make_hands.count_number++;
                 i++;
             }
 
